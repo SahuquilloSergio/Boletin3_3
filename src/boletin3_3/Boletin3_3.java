326716bd03ef3042e460aae16d87f9a5342f15e9
Sauky <Sauky@DESKTOP-2HUBGV7>
2017-10-23 19:48:06 +0200

    Commit Inicial

---
+++ src/boletin3_3/Boletin3_3.java
@@ -0,0 +1,16 @@
+package boletin3_3;
+import java.util.Scanner;
+public class Boletin3_3 {
+
+    public static void main(String[] args) {
+        circulo Circulo1 = new circulo();
+        Scanner sc = new Scanner(System.in);
+        
+
+        Circulo1.setRadio();
+        Circulo1.getArea();
+        Circulo1.getLonxitudeCircunferencia();
+    
+    }
+    
+}
